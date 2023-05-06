98ac55d8e392b9e77bba41cd333dc9387e86f2e5
Manjil Galle <manjilgalle20@gmail.com>
2023-05-06 08:47:40 +0530

    Basic variable and datatype
    Basic variable and datatype

---
+++ hello.js
@@ -0,0 +1,4 @@
+console.log("Manjil Galle..");
+console.log("Manjil Sharadrao Galle");
+console.log("I love only JS");
+console.log("Manjil Galle 22 At.Post Shendola kahurd Tq. Teosa Dist Amravati 444902 Male No")
